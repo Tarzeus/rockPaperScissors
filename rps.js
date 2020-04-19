ce3a2cee544f177ac1da8b3e91209e1e1893969e
Aaron <nbaaron13@yahoo.com>
2020-04-19 17:09:56 -0500

    Finished code for console, html and css complete, need to add functionality to webpage

--- rps.js
+++ rps.js
@@ -12,30 +12,36 @@ function oneRound(playerSelection, computerSelection){
     let lowerCase = playerSelection.toLowerCase();
     var computerSelection = computerPlay();
            if (lowerCase == 'paper' && computerSelection == 'Scissors'){
-        return "You lose! Scissors beats paper.";
+        return "You Lose! Scissors beats paper. " + "computers score is " + (computerScore+=1);
     } else if (lowerCase =='paper' && computerSelection == 'Rock'){
-        return "You Win! Paper beats Rock!";
+        return "You Win! Paper beats Rock! " + "players score is " + (playerScore+=1);
     } else if (lowerCase == 'paper' && computerSelection == 'Paper') {
-        return "It is a tie!";
+        return "It is a Tie! Paper is equal.";
     }   else if (lowerCase == 'rock' && computerSelection == 'Paper'){
-        return "You lose! Paper beats Rock.";
+        return "You Lose! Paper beats Rock. " + "computers score is " + (computerScore+=1) ;
     } else if (lowerCase =='rock' && computerSelection == 'Scissors'){
-        return "You Win! Rock beats Scissors!";        
+        return "You Win! Rock beats Scissors! " + "players score is " +  (playerScore+=1);        
     } else if (lowerCase == 'rock' && computerSelection == 'Rock') {
-        return "It is a tie!";
+        return "It is a Tie! Rocks are equal.";
     } else if (lowerCase == 'scissors' && computerSelection == 'Rock'){
-        return "You lose! Rock beats Scissors.";
+        return "You Lose! Rock beats Scissors. " + "computers score is " + (computerScore+=1);
     } else if (lowerCase =='scissors' && computerSelection == 'Paper'){
-        return "You Win! Scissors beats Paper!";
+        return "You Win! Scissors beats Paper! " + "players score is " + (playerScore+=1);
     } else if (lowerCase == 'scissors' && computerSelection == 'Scissors') {
-        return "It is a tie!";
+        return "It is a Tie! Scissors are equal.";
+    } else if ((lowerCase !== 'scissors' || lowerCase !== 'rock' || lowerCase !== 'paper')){
+        return "Invalid selection, please try again."
     }
 };
 
 function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let match = oneRound();
-
-}
-console.log(oneRound('rock'));
+    let result = prompt('msg');
+    console.log(oneRound(result));
+    if (playerScore > computerScore){
+        return "You are Winning! " + playerScore + ' to ' + computerScore;
+   } else if (playerScore < computerScore){
+       return "The computer is Winning! " + computerScore + ' to ' + playerScore;
+   } else if (playerScore === computerScore){
+       return "You are currently tied for the lead " + playerScore + ' to ' + computerScore;
+   }
+};
