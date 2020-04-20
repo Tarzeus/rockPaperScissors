c14fd58b0b43682a13f6e88307397764d770ceaa
Aaron <nbaaron13@yahoo.com>
2020-04-19 19:46:35 -0500

    FINISHED

--- rps.js
+++ rps.js
@@ -2,6 +2,7 @@ let selectMove = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let computerScore = 0;
 
+
 function computerPlay(){
     let randomIndex = Math.floor(Math.random() * selectMove.length);
     let randomIndexText = selectMove[randomIndex];
@@ -12,36 +13,49 @@ function oneRound(playerSelection, computerSelection){
     let lowerCase = playerSelection.toLowerCase();
     var computerSelection = computerPlay();
            if (lowerCase == 'paper' && computerSelection == 'Scissors'){
-        return "You Lose! Scissors beats paper. " + "computers score is " + (computerScore+=1);
+            document.getElementById("roundMsg").textContent = "You Lose! Scissors beats paper. " + "computers score is " + (computerScore+=1);
     } else if (lowerCase =='paper' && computerSelection == 'Rock'){
-        return "You Win! Paper beats Rock! " + "players score is " + (playerScore+=1);
+        document.getElementById("roundMsg").textContent = "You Win! Paper beats Rock! " + "players score is " + (playerScore+=1);
     } else if (lowerCase == 'paper' && computerSelection == 'Paper') {
-        return "It is a Tie! Paper is equal.";
+        document.getElementById("roundMsg").textContent = "It is a Tie! Paper is equal.";
     }   else if (lowerCase == 'rock' && computerSelection == 'Paper'){
-        return "You Lose! Paper beats Rock. " + "computers score is " + (computerScore+=1) ;
+        document.getElementById("roundMsg").textContent = "You Lose! Paper beats Rock. " + "computers score is " + (computerScore+=1) ;
     } else if (lowerCase =='rock' && computerSelection == 'Scissors'){
-        return "You Win! Rock beats Scissors! " + "players score is " +  (playerScore+=1);        
+        document.getElementById("roundMsg").textContent = "You Win! Rock beats Scissors! " + "players score is " +  (playerScore+=1);        
     } else if (lowerCase == 'rock' && computerSelection == 'Rock') {
-        return "It is a Tie! Rocks are equal.";
+        document.getElementById("roundMsg").textContent = "It is a Tie! Rocks are equal.";
     } else if (lowerCase == 'scissors' && computerSelection == 'Rock'){
-        return "You Lose! Rock beats Scissors. " + "computers score is " + (computerScore+=1);
+        document.getElementById("roundMsg").textContent = "You Lose! Rock beats Scissors. " + "computers score is " + (computerScore+=1);
     } else if (lowerCase =='scissors' && computerSelection == 'Paper'){
-        return "You Win! Scissors beats Paper! " + "players score is " + (playerScore+=1);
+        document.getElementById("roundMsg").textContent = "You Win! Scissors beats Paper! " + "players score is " + (playerScore+=1);
     } else if (lowerCase == 'scissors' && computerSelection == 'Scissors') {
-        return "It is a Tie! Scissors are equal.";
+        document.getElementById("roundMsg").textContent = "It is a Tie! Scissors are equal.";
     } else if ((lowerCase !== 'scissors' || lowerCase !== 'rock' || lowerCase !== 'paper')){
-        return "Invalid selection, please try again."
+        document.getElementById("roundMsg").textContent = "Invalid selection, please try again."
     }
 };
 
 function game(){
-    let result = prompt('msg');
+    let result = prompt('Rock, Paper, or Scissors? First to five wins!');
     console.log(oneRound(result));
+    document.getElementById("p1Button").innerHTML = playerScore;
+    document.getElementById("cpuButton").innerHTML = computerScore;
     if (playerScore > computerScore){
-        return "You are Winning! " + playerScore + ' to ' + computerScore;
+        document.getElementById("resultMsg").textContent = "You are Winning! " + playerScore + ' to ' + computerScore;
    } else if (playerScore < computerScore){
-       return "The computer is Winning! " + computerScore + ' to ' + playerScore;
+        document.getElementById("resultMsg").textContent = "The computer is Winning! " + computerScore + ' to ' + playerScore;
    } else if (playerScore === computerScore){
-       return "You are currently tied for the lead " + playerScore + ' to ' + computerScore;
+        document.getElementById("resultMsg").textContent = "You are currently tied for the lead " + playerScore + ' to ' + computerScore;
+   }
+   if (playerScore === 5){
+    document.getElementById("resultMsg").textContent = "YOU WON! Please refresh to play again."
+   } else if (computerScore === 5){
+    document.getElementById("resultMsg").textContent = "YOU LOSE! Please refresh to play again."
    }
 };
+
+/*
+document.getElementById("resultMsg").innerHTML = 
+
+
+document.getElementById("roundMsg").textContent =*/
